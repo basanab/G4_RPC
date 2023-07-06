68d2079a78bb031dc222f6bdf492f52fb0cf6fda
basanab <125974371+basanab@users.noreply.github.com>
2023-07-06 12:24:34 +0530

    Add files via upload

---
+++ RPC_Build/CMakeFiles/mical_g4.dir/DependInfo.cmake
@@ -0,0 +1,53 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/basana/simulation/ical_g4/main.cpp" "/home/basana/simulation/ical-build/CMakeFiles/mical_g4.dir/main.cpp.o"
+  "/home/basana/simulation/ical_g4/src/EventAction.cc" "/home/basana/simulation/ical-build/CMakeFiles/mical_g4.dir/src/EventAction.cc.o"
+  "/home/basana/simulation/ical_g4/src/G01ActionInitialization.cc" "/home/basana/simulation/ical-build/CMakeFiles/mical_g4.dir/src/G01ActionInitialization.cc.o"
+  "/home/basana/simulation/ical_g4/src/G01GeneralPrimaryGeneratorAction.cc" "/home/basana/simulation/ical-build/CMakeFiles/mical_g4.dir/src/G01GeneralPrimaryGeneratorAction.cc.o"
+  "/home/basana/simulation/ical_g4/src/G01PrimaryGeneratorAction.cc" "/home/basana/simulation/ical-build/CMakeFiles/mical_g4.dir/src/G01PrimaryGeneratorAction.cc.o"
+  "/home/basana/simulation/ical_g4/src/RunAction.cc" "/home/basana/simulation/ical-build/CMakeFiles/mical_g4.dir/src/RunAction.cc.o"
+  "/home/basana/simulation/ical_g4/src/SteppingAction.cc" "/home/basana/simulation/ical-build/CMakeFiles/mical_g4.dir/src/SteppingAction.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "G4LIB_BUILD_DLL"
+  "G4UI_USE_QT"
+  "G4VIS_USE_OPENGL"
+  "G4VIS_USE_OPENGLQT"
+  "G4VIS_USE_OPENGLX"
+  "G4VIS_USE_TOOLSSG_QT_GLES"
+  "G4VIS_USE_TOOLSSG_X11_GLES"
+  "PTL_BUILD_DLL"
+  "QT_CORE_LIB"
+  "QT_GUI_LIB"
+  "QT_NO_DEBUG"
+  "QT_OPENGL_LIB"
+  "QT_PRINTSUPPORT_LIB"
+  "QT_WIDGETS_LIB"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/home/basana/simulation/ical_g4/include"
+  "/DATA/software/2023/GEANT4/install/include/Geant4"
+  "/usr/include/qt5"
+  "/usr/include/qt5/QtCore"
+  "/usr/lib64/qt5/./mkspecs/linux-g++"
+  "/usr/include/qt5/QtGui"
+  "/usr/include/qt5/QtWidgets"
+  "/usr/include/qt5/QtOpenGL"
+  "/usr/include/qt5/QtPrintSupport"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ RPC_Build/CMakeFiles/mical_g4.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mical_g4.dir/main.cpp.o"
+  "CMakeFiles/mical_g4.dir/src/EventAction.cc.o"
+  "CMakeFiles/mical_g4.dir/src/G01ActionInitialization.cc.o"
+  "CMakeFiles/mical_g4.dir/src/G01GeneralPrimaryGeneratorAction.cc.o"
+  "CMakeFiles/mical_g4.dir/src/G01PrimaryGeneratorAction.cc.o"
+  "CMakeFiles/mical_g4.dir/src/RunAction.cc.o"
+  "CMakeFiles/mical_g4.dir/src/SteppingAction.cc.o"
+  "mical_g4"
+  "mical_g4.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/mical_g4.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
